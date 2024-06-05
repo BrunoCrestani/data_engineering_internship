83de2eab7e3f6f64dc6216da362bb9e0d1d1c757
BrunoCrestani <bruno.crestani@ufpr.br>
2024-06-05 14:31:35 -0300

    updates on data cleaning

--- eda/data_cleaning.py
+++ eda/data_cleaning.py
@@ -2,66 +2,123 @@ import os
 import pandas as pd
 
 class DataCleaning:
-
     def __init__(self, project_dir):
         self.data_dir = project_dir 
         if not os.path.isdir(self.data_dir):
-            raise FileNotFoundError(f"O diretorio {self.data_dir} nao existe.")
+            raise FileNotFoundError(f"The directory {self.data_dir} does not exist.")
+        print(f"Initialized DataCleaning with directory: {self.data_dir}")
 
-    # method to load the files into the dataframes
+    # Method to load the files into the dataframes
     def load_csv_files(self):
         csv_files = []
-
-        #iterates through the directories
+        # Iterates through the directories
         for root, _, files in os.walk(self.data_dir):
             for file in files:
                 if file.endswith('.csv'):
                     csv_files.append(os.path.join(root, file))
 
-        dataframes = {os.path.basename(file): pd.read_csv(file) for file in csv_files}
+        dataframes = {file: pd.read_csv(file) for file in csv_files}
+        print(f"Loaded {len(dataframes)} CSV files.")
         return dataframes
 
-    # methods to clean useless data
+    # Method to clean useless data
     def clean_data(self, dataframe):
-
-        dataframe = dataframe.drop(index=dataframe.index[-1])
-
-        # excludes empty lines
+        # Excludes empty lines
         dataframe = dataframe.dropna(how='all')
         dataframe = dataframe.dropna(axis=1, how='all')
 
-        # makes columns names prettier
+        # Makes columns names prettier
         dataframe.columns = dataframe.columns.str.strip().str.lower().str.replace('_ ', ' ')
 
+        # Check and remove the last row if it starts with specific words
+        last_row_first_item = str(dataframe.iloc[-1, 0]).strip()
+        if not last_row_first_item or last_row_first_item.startswith(("Fonte", "Nota", "(")):
+            print(f"last row removed: {last_row_first_item}")
+            dataframe = dataframe.iloc[:-1, :]
+        
         return dataframe
 
-    #save the csv cleaned files
-    def save_clean_data(self, dataframes, output_dir):
-        # verifies if the output directory already existis
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
+    # Method to separate a sheet with 2 tables in diferent dataframes
+    def separate_sheets(self, dataframe):
+        # Arrray of found tables in the dataframes
+        array = [col for col in dataframe.columns if col.startswith('tab')]
+       
+        # Verifies if there is more than 1
+        if len(array) < 2:
+            return [dataframe]
+        else:
+            # Separates the tables
+            start_column = array[1]
+            index = dataframe.columns.get_loc(start_column)
+            new_df = dataframe.iloc[:, index:].copy()
+            dataframe = dataframe.iloc[:, :index]
 
+            dataframes = [dataframe, new_df]
+            return dataframes
+
+    # Method to reorganize the files and Tables names
+    def reorganize_dfs(self, dataframes):
+        # Iterates through dataframes looking for the ones to be separated
+        for file_path, df in dataframes.items():
+            separated_dfs = self.separate_sheets(df)
+            # Defining the paths of the original file for use
+            directory = os.path.dirname(file_path)
+            base_filename = os.path.basename(file_path)
+            filename_without_extension = os.path.splitext(base_filename)[0]
+
+            # Correcting files names
+            if filename_without_extension.endswith(' '):
+                filename_without_extension = filename_without_extension[:-1]
+
+            print(f"Filename without extension: {filename_without_extension}")
+
+            # Process to correct the name of the arrays with separated dfs received
+            if len(separated_dfs) > 1:
+                if ' e ' in filename_without_extension:
+                    file_name_parts = filename_without_extension.split(' e ')
+                    print(f"Split into: {file_name_parts}")
+                    part1_path = os.path.join(directory, file_name_parts[0] + '.csv')
+                    part2_path = os.path.join(directory, file_name_parts[1] + '.csv')
+                    separated_dfs[0].to_csv(part1_path, index=False)
+                    separated_dfs[1].to_csv(part2_path, index=False)
+                    print(f"Saved split files to: {part1_path}, {part2_path}")
+                elif ' a ' in filename_without_extension:
+                    file_name_parts = filename_without_extension.split(' a ')
+                    print(f"Split into: {file_name_parts}")
+                    part1_path = os.path.join(directory, file_name_parts[0] + '.csv')
+                    part2_path = os.path.join(directory, file_name_parts[1] + '.csv')
+                    separated_dfs[0].to_csv(part1_path, index=False)
+                    separated_dfs[1].to_csv(part2_path, index=False)
+                    print(f"Saved split files to: {part1_path}, {part2_path}")
+
+                os.remove(file_path)
+            else:
+                print(f"No split needed for: {file_path}")
+                separated_dfs[0].to_csv(file_path, index=False)
+                print(f"Saved file to: {file_path}")
+
+    # Save the csv cleaned files
+    def save_clean_data(self, dataframes):
         # Iterates over the dataframes
-        for filename, df in dataframes.items():
-
-            relative_path = os.path.relpath(filename, start=self.data_dir)
-            
+        for file_path, df in dataframes.items():
             # Clean the DataFrame
             clean_df = self.clean_data(df)
             # Saves the dataframe
-            clean_df.to_csv(os.path.join(output_dir, os.path.basename((filename))), index=False)
+            clean_df.to_csv(file_path, index=False)
+            print(f"Saved cleaned data to: {file_path}")
 
 def main():
     data_dir = os.path.join(os.getcwd(), 'csv_files')
     project_dir = data_dir
-    output_dir = os.path.join(os.getcwd(), 'cleaned_csv_files')
 
     cleaner = DataCleaning(project_dir)
     dataframes = cleaner.load_csv_files()
 
     cleaned_dataframes = {filename: cleaner.clean_data(df) for filename, df in dataframes.items()} 
-    
-    cleaner.save_clean_data(cleaned_dataframes, output_dir)
+
+    saved_dataframes = cleaner.save_clean_data(cleaned_dataframes)
+    cleaner.reorganize_dfs(cleaned_dataframes)
 
 if __name__ == "__main__":
     main()
+
--- eda/data_visualization.py
+++ eda/data_visualization.py
@@ -3,32 +3,32 @@ import streamlit as st
 import pandas as pd
 
 class DataVisualizer:
-
-    def __init__(self, cleaned_data_dir, data_example):
+    def __init__(self, cleaned_data_dir):
         self.cleaned_data_dir = cleaned_data_dir
-        self.data_example = data_example
 
-    # method to example a visualization of a treated sheet
-    def visualize_data(self):
+    # Method to visualize a specific example sheet
+    def visualize_data_example(self, data_example):
+        # Load the example CSV file
+        df = pd.read_csv(data_example)
+        
+        # Set the title and subheader
+        st.title(df.iloc[0, 0])
+        st.subheader(df.iloc[0, 1])
 
-        #treating sheet specifically
-        df = pd.read_csv(self.data_example)
-        st.title = (df.iloc[0,0])
-        st.subheader(df.iloc[0,1])
+        # Reload the DataFrame skipping the first three rows
+        df = pd.read_csv(data_example, skiprows=3)
 
-        df = pd.read_csv(self.data_example, skiprows=3)
-
+        # Rename columns
         column0 = df.columns[0]
         column1 = df.columns[1]
         
-        df.rename(columns={column0: ''}, inplace=True)
-        df.rename(columns={column1: 'Brasil'}, inplace=True)
+        df.rename(columns={column0: '', column1: 'Brasil'}, inplace=True)
         st.write(df)
 
     def load_cleaned_data(self):
         cleaned_data = {}
 
-        # Iterates through the directory
+        # Iterates through the directory to load cleaned data
         for root, _, files in os.walk(self.cleaned_data_dir):
             for file in files:
                 if file.endswith('.csv'):
@@ -38,8 +38,6 @@ class DataVisualizer:
         return cleaned_data
 
     def display_sheets(self):
-
-        plt.switch_backend('TkAgg')
         st.title('Data Visualization with Streamlit')
 
         # Load cleaned data
@@ -47,18 +45,16 @@ class DataVisualizer:
 
         # Display each cleaned DataFrame
         for filename, df in cleaned_data.items():
-            st.header(filename)
+            st.subheader(f'Data from {filename}')
             st.write(df)
 
 def main():
-
-    cleaned_data_dir = os.path.join(os.getcwd(), 'cleaned_csv_files')
+    cleaned_data_dir = os.path.join(os.getcwd(), 'csv_files')
     data_example = os.path.join(cleaned_data_dir, 'Tab 1.1.18.3.csv')
     
-    data_visualizer = DataVisualizer(cleaned_data_dir, data_example)
-    data_visualizer.visualize_data()
+    data_visualizer = DataVisualizer(cleaned_data_dir)
+    data_visualizer.display_sheets()
 
 if __name__ == "__main__":
     main()
-
 
